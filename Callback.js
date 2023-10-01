9a71bd3561b9e1e4f576679a72366c1c881856dc
surajchoudh <surajchoudhary604@gmail.com>
2023-10-01 15:26:22 +0530

    callback

--- Callback.js
+++ Callback.js
@@ -15,9 +15,5 @@ countdown(10, function () {
     const countdownContainer = document.getElementById('countdown-container');
     countdownContainer.textContent = "Happy Independence Day";
 });
-Now, you have the HTML structure in countdown.html, CSS styles in styles.css, and JavaScript logic in script.js.Open countdown.html in a web browser to see the countdown from 10 to 1, followed by the message "Happy Independence Day".The countdown and message will be styled according to the CSS rules specified in styles.css.
-
-
-
 
 
